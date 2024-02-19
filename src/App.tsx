eb85a92b68f6c777756f23ef038833e0ffaa9a03
cedricngoune <gabyngoune@yahoo.fr>
2024-02-19 10:55:54 +0100

    template ready

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl text-center ">
+        Scaffolding React + Vite + Tailwind project
+      </h1>
+    </>
+  );
+}
+
+export default App;
